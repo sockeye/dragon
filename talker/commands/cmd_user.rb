2d9b12a1a538241bda3c9021cf10fe9430b0f8cf
TheBear <james@forluna.co.uk>
2010-01-10 11:05:26 +0000

    New commands prompt, colour, recap

--- talker/commands/cmd_user.rb
+++ talker/commands/cmd_user.rb
@@ -16,6 +16,7 @@ module Commands
       self.title = message.slice(0, 60)
       output "Your title is now:\n#{name} #{title}^n"
     end
+    save
   end
   
   define_command 'gender' do |message|
@@ -32,8 +33,49 @@ module Commands
     if changed
       output "Your gender has been set to #{gender_text}."
     else
-      output "Your gender is #{gender_text}, to changed it type ^Lgender <female|male|none>^n"
+      output "Your gender is #{gender_text}, to change it type ^Lgender <female|male|none>^n"
+    end
+    save
+  end
+
+  define_command 'prompt' do |message|
+    if message.blank?
+      self.prompt = nil
+      output "You will now receive the default prompt"
+    elsif message == "off"
+      self.prompt = ""
+      output "You will no longer receive a prompt"
+    else
+      self.prompt = message
+      output "Prompt changed"
+    end
+    save
+  end
+  
+  define_command 'colour' do |message|
+    if message == "on"
+      self.colour = :ansi
+      output "^YColour output is on!^n"
+    elsif message == "off"
+      self.colour = :off
+      output "Colour output is off"
+    elsif message == "wands"
+      self.colour = :wands
+      output "You are now viewing colour wands."
+    else message.blank?
+      output "Format: colour [on|off|wands]"
+    end
+    save
+  end
+  define_alias 'colour', 'color'
+  
+  define_command 'recap' do |message|
+    if message.blank? || message.downcase != lower_name
+      output "Format: recap <your username in lower or uppercase letters>"
+    else
+      self.name = message
+      output "Your name is now capitalised as #{name}."
+      save
     end
   end
-      
 end
