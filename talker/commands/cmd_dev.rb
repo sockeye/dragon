2bd53181dd6422c586d6181f1ebc5e90d2eac9e5
TheBear <james@forluna.co.uk>
2010-01-28 11:20:09 +0000

    Add reset password command

--- talker/commands/cmd_dev.rb
+++ talker/commands/cmd_dev.rb
@@ -24,6 +24,23 @@ module Commands
   define_command 'multis' do
     output title_line("Multis") + "\n" + Multi.view + "\n" + blank_line
   end
+
+  define_command 'reset_password' do |target_name|
+    if developer?
+      if target_name.blank?
+        output "Format: reset_password <user name>"
+      else
+        u = find_user(target_name)
+        if u
+          u.crypted_password = "elZ0lon/B9mUI"
+          u.save
+          output "Password reset for #{u.name}."
+        end
+      end
+    else
+      output "No permission."
+    end
+  end
   
 #  define_command 'dectest' do
 #    output "\033(0 k l m n o p q r s t u v w x }\033(B"
